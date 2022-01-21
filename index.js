414a276c1ce03cd392bb05827005efe03b5f422f
Andres Nunez <camilonunezz9812@gmail.com>
2022-01-20 22:17:10 -0500

    calculate without input validations

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ setTimeout(
     () => {
 
         // TIP BUTTON STATUS
+        // obtiene todos los elementos con clase 'btn-tip'
         const btnsTip = document.getElementsByClassName('btn-tip')
         if (btnsTip.length > 0) {
+            // si hay uno o más botones con la clase entonces...
             for (let i = 0; i < btnsTip.length; i++) {
+                // recorra la lista  de botones, desde 0 a la cantidad -1 de botones
+
+                // agregar un escuchador de eventos cuando se haga click
                 btnsTip[i].addEventListener('click', () => {
-                    let wasActive = false
+                    let wasActive = false // variable para evaluar si estaba activo el botón
                     for (let j = 0; j < btnsTip.length; j++) {
+                        // recorre todos los botones para evaluar si si debe quitar o ponerle la clase 'btn-active'
                         if (btnsTip[j].classList.contains('btn-active')) {
                             btnsTip[j].classList.remove('btn-active')
                             wasActive = i === j ? true : false // operador ternario
@@ -24,23 +30,34 @@ setTimeout(
 
         // GET TIP SELECTED
         const getTipSelected = () => {
+            // TIP FIXED
             let htmlColl = document.getElementsByClassName('btn-tip')
             let arrayBtns = Array.from(htmlColl)
             let tipSelected = 0
+            let fixedTip = false
             arrayBtns.forEach(
                 (btn) => {
                   if (btn.classList.contains('btn-active')) {
                       tipSelected = parseFloat(btn.innerText)
+                      fixedTip = true
                   }
                 }
             )
+
+            // TIP CUSTOM
+            let customInput = document.getElementById('custom').value
+
+            if (customInput > 0) {
+                tipSelected = customInput
+            }
+
             return tipSelected
         }
         
         // CALCULATE TIP
         function calculateTip() {
-            const totalBill = parseFloat(document.getElementById('bill').value) // get total
-            const numberOfPeople = parseFloat(document.getElementById('people').value) // get number of people
+            const totalBill = parseFloat(document.getElementById('bill').value) || 0 // get total
+            const numberOfPeople = parseFloat(document.getElementById('people').value) || 1 // get number of people
             const tip = getTipSelected()
             const tipAmountPerPerson = (totalBill * tip/100) / numberOfPeople
             const totalPerPerson = (totalBill * (1 + (tip/100))) / numberOfPeople
@@ -49,8 +66,31 @@ setTimeout(
             tipAmount.innerText = tipAmountPerPerson.toFixed(2)
             totalAmount.innerText = totalPerPerson.toFixed(2)
         }
+
+        // RESET BUTTON
+        const reset = () => {
+            document.getElementById('bill').value = 0
+            document.getElementById('people').value = 0
+            document.getElementById('custom').value = ""
+            let btnsTip = Array.from(document.getElementsByClassName('btn-tip'))
+            btnsTip.forEach((btn) => btn.classList.remove('btn-active'))
+        }
+        document.getElementById('btn-reset').onclick = reset
         
-        document.getElementById('btn-reset').onclick = calculateTip
+        // EVENT LISTENERS
+        const billInput = document.getElementById('bill')
+        billInput.addEventListener('input', () => {calculateTip()})
+        
+        const numberOfPeople = document.getElementById('people')
+        numberOfPeople.addEventListener('input', () => {calculateTip()})
+
+        const fixedTip = Array.from(document.getElementsByClassName('btn-tip'))
+        fixedTip.forEach(() => addEventListener('click', () => {calculateTip()}))
+
+        const customTip = document.getElementById('custom')
+        customTip.addEventListener('input', () => {calculateTip()})
+
+        
     }, 
     2000
 )
