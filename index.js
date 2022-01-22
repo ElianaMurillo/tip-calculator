42c00e22497012d1745d98906c8b45505cecd1e7
Andres Nunez <camilonunezz9812@gmail.com>
2022-01-21 21:38:24 -0500

    removed array

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ setTimeout(
     () => {
 
         var allowedDigits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-        var allowedChars = ['.', ',']
 
         // TIP BUTTON STATUS
         // obtiene todos los elementos con clase 'btn-tip'
